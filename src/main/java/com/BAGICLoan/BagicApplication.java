96822f358ca6bb07577afdb5fc6d86ad5a9d4004
vijayasreem <133225735+vijayasreem@users.noreply.github.com>
2024-03-20 14:52:26 +0530

    initial commit

---
+++ src/main/java/com/BAGICLoan/BagicApplication.java
@@ -0,0 +1,13 @@
+package com.BAGICLoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BAGICLoan/BagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BAGICLoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
